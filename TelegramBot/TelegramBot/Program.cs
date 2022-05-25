e03464248431941b19996a986252d5eb4fc4d342
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2022-05-25 16:47:34 +0300

    Создан проект

---
+++ TelegramBot/TelegramBot/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+
+
+namespace TelegramBot
+{
+    class TelegramBot
+    {
+
+    }
+}
