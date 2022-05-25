39d9a1796b3a835c1d2c629e6d44b809d980dfd6
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2022-05-25 23:10:14 +0300

    Бот научился отправлять картинку

--- TelegramBot/TelegramBot/Program.cs
+++ TelegramBot/TelegramBot/Program.cs
@@ -25,22 +25,27 @@ namespace TelegramBot
 
                 var hashHelloArr = new HashSet<string>(HelloArr);
                 var hashWhatsUpArr = new HashSet<string>(WhatsUpArr);
-
+          
                 StreamWriter DataBase = new StreamWriter("E:\\DataBase.txt", true);
                 DataBase.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
                 DataBase.Close();
 
                 if (hashHelloArr.Contains(message.Text))
                 {
-                    await botClient.SendTextMessageAsync(message.Chat, "Привет!");
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Привет!");
                     return;
                 }
                 else if (hashWhatsUpArr.Contains(message.Text))
                 {
-                    await botClient.SendTextMessageAsync(message.Chat, "Хорошо, у тебя как?");
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Хорошо, у тебя как?");
                     return;
                 }
-                await botClient.SendTextMessageAsync(message.Chat, "Я не знаю как ответить на это :(");
+                else if (message.Text == "/getimage")
+                {
+                    await bot.SendPhotoAsync(message.Chat.Id, "https://avatarko.ru/img/kartinka/33/Star_Wars_Darth_Vader_32632.jpg");
+                    return;
+                }
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Я не знаю как ответить на это :(");
             }
         }
 
